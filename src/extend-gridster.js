06fa1b74cbf3f0df97418b410f53e04fa7d3d69f
p-kudrin <bazzybon@gmail.com>
2015-08-18 11:42:58 +0600

    added files to extend gridster

---
+++ src/extend-gridster.js
@@ -0,0 +1,161 @@
+;(function(root, factory) {
+
+    root.Gridster = factory(root.$ || root.jQuery, root.Gridster);
+
+}(this, function($, Gridster) {
+
+    Gridster.prototype.manage_movements = function($widgets, to_col, to_row) {
+        $.each($widgets, $.proxy(function(i, w) {
+            var wgd = w;
+            var $w = wgd.el;
+            var y = (to_row + this.player_grid_data.size_y) - wgd.row;
+
+            this.move_widget_down($w, y);
+            this.set_placeholder(to_col, to_row);
+        }, this));
+
+        return this;
+    };
+
+    Gridster.prototype.set_placeholder = function(col, row) {
+        var phgd = $.extend({}, this.placeholder_grid_data);
+        var $nexts = this.widgets_below({
+                col: phgd.col,
+                row: phgd.row,
+                size_y: phgd.size_y,
+                size_x: phgd.size_x
+            });
+
+        // Prevents widgets go out of the grid
+        var right_col = (col + phgd.size_x - 1);
+        if (right_col > this.cols) {
+            col = col - (right_col - col);
+        }
+
+        var moved_down = this.placeholder_grid_data.row < row;
+        var changed_column = this.placeholder_grid_data.col !== col;
+
+        this.placeholder_grid_data.col = col;
+        this.placeholder_grid_data.row = row;
+
+        this.cells_occupied_by_placeholder = this.get_cells_occupied(
+            this.placeholder_grid_data);
+
+        this.$preview_holder.attr({
+            'data-row' : row,
+            'data-col' : col
+        });        
+
+        var $widgets_under_ph = this.get_widgets_under_player(
+            this.cells_occupied_by_placeholder);
+
+        if ($widgets_under_ph.length) {
+            $widgets_under_ph.each($.proxy(function(i, widget) {
+                var $w = $(widget);
+                this.move_widget_down(
+                 $w, row + phgd.size_y - $w.data('coords').grid.row);
+            }, this));
+        }
+
+    };
+
+    Gridster.prototype.on_overlapped_row_change = function(start_callback, end_callback) {
+        if (!this.colliders_data.length) {
+            return this;
+        }
+        (start_callback || $.noop).call(this, this.colliders_data[0].el.data.row);
+    };
+
+    Gridster.prototype.on_overlapped_column_change = function(start_callback, stop_callback) {
+        if (!this.colliders_data.length) {
+            return this;
+        }
+        (start_callback || $.noop).call(this, this.colliders_data[0].el.data.col);
+    };
+
+    Gridster.prototype.on_stop_overlapping_column = function(col) {
+
+    };
+
+    Gridster.prototype.on_stop_overlapping_row = function(row) {
+
+    };
+
+    Gridster.prototype.add_to_gridmap = function(grid_data, value) {
+        this.update_widget_position(grid_data, value || grid_data.el);
+    };
+
+    Gridster.prototype.register_widget = function($el) {
+        var isDOM = $el instanceof jQuery;
+        var wgd = isDOM ? this.dom_to_coords($el) : $el;
+        var posChanged = false;
+        isDOM || ($el = wgd.el);        
+
+        if (this.options.avoid_overlapped_widgets &&
+            !this.can_move_to(
+             {size_x: wgd.size_x, size_y: wgd.size_y}, wgd.col, wgd.row)
+        ) {
+            $.extend(wgd, this.next_position(wgd.size_x, wgd.size_y));
+            $el.attr({
+                'data-col': wgd.col,
+                'data-row': wgd.row,
+                'data-sizex': wgd.size_x,
+                'data-sizey': wgd.size_y
+            });
+            posChanged = true;
+        }
+
+        // attach Coord object to player data-coord attribute
+        $el.data('coords', $el.coords());
+        // Extend Coord object with grid position info
+        $el.data('coords').grid = wgd;
+
+        this.add_to_gridmap(wgd, $el);
+
+        this.options.resize.enabled && this.add_resize_handle($el);
+
+        return posChanged;
+    };
+
+    Gridster.prototype.set_player = function(col, row, no_player) {
+        var self = this;
+        if (!no_player) {
+            this.empty_cells_player_occupies();
+        }
+        var cell = !no_player ? self.colliders_data[0].el.data : {col: col};
+        var to_col = cell.col;
+        var to_row = row || cell.row;
+
+        this.player_grid_data = {
+            col: to_col,
+            row: to_row,
+            size_y : this.player_grid_data.size_y,
+            size_x : this.player_grid_data.size_x
+        };
+
+        this.cells_occupied_by_player = this.get_cells_occupied(
+            this.player_grid_data);
+
+        var $overlapped_widgets = this.get_widgets_overlapped(
+            this.player_grid_data);
+
+        var constraints = this.widgets_constraints($overlapped_widgets);
+
+        this.manage_movements(constraints.can_go_up, to_col, to_row);
+        this.manage_movements(constraints.can_not_go_up, to_col, to_row);
+
+        /* if there is not widgets overlapping in the new player position,
+         * update the new placeholder position. */
+        if (!$overlapped_widgets.length) {            
+            this.set_placeholder(to_col, to_row);
+        }
+
+        return {
+            col: to_col,
+            row: to_row
+        };
+    };
+
+
+    return Gridster;
+}));
